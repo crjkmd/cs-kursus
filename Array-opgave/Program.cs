4a019a710a667caebdad5a9e8b772cb82d87c29c
ChristianJepsen <jepsen.chr@gmail.com>
2018-05-23 15:30:41 +0200

    Array opgave

---
+++ Array-opgave/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var rest = BeregnOgSorterArray(test);
+            Console.WriteLine(rest);
+            Console.ReadLine();
+        }
+        public static ArrayResultat BeregnOgSorterArray(int[] a)
+        {
+            ArrayResultat r;
+            r.sum = 0;
+            foreach (var item in a)
+            {
+                r.sum += item;
+            }
+            r.gennemsnit = r.sum / a.Length;
+            Array.Sort(a);
+            return r;
+        }
+    }
+    struct ArrayResultat
+    {
+        public double sum;
+        public double gennemsnit;
+    }
+}
