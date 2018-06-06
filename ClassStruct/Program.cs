7bb550e536bf8fb17bb226fffde675e3b7aa3281
Christian Jepsen <jepsen.chr@gmail.com>
2018-06-06 09:09:51 +0200

    Added Classe Stuct project

---
+++ ClassStruct/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person k1 = new Person();
+            k1.navn = "a";
+            Person k2 = new Person();
+            k2.navn = "b";
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+            Console.ReadLine();
+            k1 = k2;
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+            Console.ReadLine();
+            k1.navn = "c";
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+            Console.ReadLine();
+        }
+    }
+}
+
+/* struct Person
+{
+    public string navn;
+}
+*/
+class Person
+{
+    public string navn;
+}
