88763a38a02359c00cb1d6b2cb95e14e7b0f43b6
Christian Jepsen <jepsen.chr@gmail.com>
2018-06-07 08:48:55 +0200

    ArealBeregninger

---
+++ ArealBeregninger/ArealBeregninger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArealBeregninger
+{
+    static class ArealBeregninger
+    {
+        public static double BeregnArealFirkant(double h, double b)
+        {
+            return h * b;
+        }
+        public static double BeregnArealCirkel(double r)
+        {
+            return Math.Pow(r, 2) * System.Math.PI;
+        }
+    }
+}
---
+++ ArealBeregninger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArealBeregninger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArealBeregninger.BeregnArealFirkant(10,10));
+            Console.WriteLine(ArealBeregninger.BeregnArealCirkel(10));
+            Console.ReadLine();
+        }
+    }
+}
