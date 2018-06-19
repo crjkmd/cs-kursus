6ff0e93b3f2cbcbb04cd04eeaeb8be170e7933a8
Christian Jepsen <jepsen.chr@gmail.com>
2018-06-19 12:36:46 +0200

    BrugAfUsing

---
+++ BrugAfUsing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrugAfUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taeller = 0;
+            using (StreamReader stream = System.IO.File.OpenText(@"x:\dyrenavne.txt"))
+            {
+                while (stream.Peek() != -1)
+                {
+                    string navn = stream.ReadLine();
+                    Console.WriteLine(taeller + " " + navn.ToUpper());
+                    using (StreamWriter stream1 = System.IO.File.AppendText(@"x:\crj.txt"))
+                    {
+                        stream1.WriteLine(taeller + " " + navn.ToUpper());
+                    }
+                    taeller++;
+                }
+                //stream.Close();
+                //stream = null;
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- InterfaceSort/Program.cs
+++ InterfaceSort/Program.cs
@@ -10,9 +10,10 @@ namespace InterfaceSort
     {
         static void Main(string[] args)
         {
-            Hund[] h = new Hund[2];
+            Hund[] h = new Hund[3];
             h[0] = new Hund() { Alder = 10, Navn ="King" };
             h[1] = new Hund() { Alder = 8, Navn ="Tjuggi" };
+            h[2] = new Hund() { Alder = 8, Navn = "Fnuggi" };
             Array.Sort(h);
             foreach (var item in h)
             {
