2d1415986096931caa11045640dd427d4289781f
Christian Jepsen <jepsen.chr@gmail.com>
2018-06-19 15:05:55 +0200

    ArrayList

---
+++ BunkeKort/Bunke.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunkeKort
+{
+    public class Bunke
+    {
+        private Stack<Kort> bunke = new Stack<Kort>();
+
+        public void TilføjKort(Kort k)
+        {
+            bunke.Push(k);
+        }
+
+        public Kort Fjernkort()
+        {
+            if (bunke.Count>0)
+                return bunke.Pop();
+            return null;                
+        }
+
+        public void Vis()
+        {
+            foreach (var item in bunke)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ BunkeKort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunkeKort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bunke b = new Bunke();
+            b.TilføjKort(new Kort() { Kulør = "Spar", Værdi = 2 });
+            b.TilføjKort(new Kort() { Kulør = "Hjerter", Værdi = 14 });
+            b.TilføjKort(new Kort() { Kulør = "Ruder", Værdi = 3 });
+
+            b.Vis();
+
+            Console.ReadLine();
+
+            var k = b.Fjernkort();
+            Console.WriteLine();
+            Console.WriteLine(k);
+            Console.WriteLine();
+
+            b.Vis();
+            Console.ReadLine();
+
+            var y = b.Fjernkort();
+            Console.WriteLine("Det fjernede kort er");
+            Console.WriteLine(y);
+            Console.WriteLine("Tilbage på ArrayList er");
+            b.Vis();
+            Console.ReadLine();
+        }
+    }
+}
