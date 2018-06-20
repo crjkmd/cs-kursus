1dae7995715f8daa0d0658c9a6de8aa0ead7e7b1
Christian Jepsen <jepsen.chr@gmail.com>
2018-06-20 11:48:25 +0200

    .Delegates1

--- BrugDelegates/Program.cs
+++ BrugDelegates/Program.cs
@@ -9,18 +9,36 @@ namespace BrugDelegates
     
     class Program
     {
-        public delegate int BeregnDelegate(int a, int b);
+       // public delegate int BeregnDelegate(int a, int b);
         //public delegate void LogDelegate(string txt);
 
         static void Main(string[] args)
         {
-            BeregnDelegate s = new BeregnDelegate(Plus);
-            int res = Beregner(1, 1, s);
-            Console.WriteLine(res);
+            // BeregnDelegate s = new BeregnDelegate(Plus);
+            // int res = Beregner(1, 1, s);
+            // Console.WriteLine(res);
 
-            BeregnDelegate s1 = Minus;
-            int res2 = Beregner(1, 1, Minus);
-            Console.WriteLine(res2);
+            // BeregnDelegate s1 = Minus;
+            // int res2 = Beregner(1, 1, Minus);
+            // Console.WriteLine(res2);
+
+            int res1 = 0;
+            res1 = Beregner(3, 4, FindTilfældigFunktion());
+            Console.WriteLine(res1);
+            Console.ReadLine();
+
+            res1 = Beregner(3, 4, FindTilfældigFunktion());
+            Console.WriteLine(res1);
+            Console.ReadLine();
+
+            int res = 0;
+            res = Beregner(1, 1, Plus);
+            Console.WriteLine(res);
+            res = Beregner(1, 1, Minus);
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+            Console.WriteLine("***");
 
             int res3 = Beregner(1, 1, Gange);
             Console.WriteLine(res3);
@@ -35,11 +53,25 @@ namespace BrugDelegates
             m.Stop();
             Console.ReadLine();
         }
+
+        public static Func<int,int,int> FindTilfældigFunktion()
+        {
+            if (System.DateTime.Now.Millisecond <= 250)
+                return Plus;
+            if (System.DateTime.Now.Millisecond <= 500)
+                return Minus;
+            if (System.DateTime.Now.Millisecond <= 750)
+                return Gange;
+            return Divider;
+        }
+    
+
         public static void AppendLog(string txt)
         {
             System.IO.File.AppendAllText(@"c:\temp\log.txt", txt + "\r\n");
         }
-        public static int Beregner(int a, int b, BeregnDelegate funktion)
+        public static int Beregner(int a, int b, Func<int, int, int> funktion)
+            //public static int Beregner(int a, int b, BeregnDelegate funktion)
         {
             return funktion(a, b);
         }
