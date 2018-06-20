180fd104d5d6142b329d1061a2dee70e7293acb5
Christian Jepsen <jepsen.chr@gmail.com>
2018-06-20 14:46:11 +0200

    Lambda

---
+++ Events/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Thread thread = new Thread(new ThreadStart(WorkThread1));
+            //thread.Start();
+
+            // -- Binder kode til frameworket
+            System.Timers.Timer t = new System.Timers.Timer();
+            t.Interval = 500;
+            t.Enabled = true;
+            //t.Elapsed += Tick;
+
+            System.IO.FileSystemWatcher f = new System.IO.FileSystemWatcher(@"c:\temp");
+            f.EnableRaisingEvents = true;
+            f.Created += (s, e) => Console.WriteLine("Fil oprettet " + e.FullPath);
+            f.Deleted += (w, q) => Console.WriteLine("Fil slettet " + q.FullPath);
+            f.Renamed += (a, s) => Console.WriteLine("Fil renamet " + s.FullPath + " Filnavnet var før: " + s.OldName);
+
+            //Thread thread1 = new Thread(new ThreadStart(WorkThread1));
+            //thread1.Start();
+
+            //do
+            //{
+
+            //} while (true);
+
+
+        }
+
+        public static void WorkThread1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Thread ");
+            }
+        }
+        private static void Tick(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("Tick " + e.SignalTime);
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 5, 1, 51, 7, 1, 56, 36, 5 };
+
+            // -- Action
+            a.ForEach(Skriv);
+            Console.ReadLine();
+            a.ForEach((int x) => { Console.WriteLine(": " + x); });
+            Console.ReadLine();
+
+            // -- Find værdien 51
+            a.ForEach((int y) =>
+            {
+                if (y == 51)
+                    Console.WriteLine("Der var en y = "+y);
+                else
+                    Console.WriteLine("Dett var ikke en y=51 men = " +y);
+            });
+            
+            Console.ReadLine();
+
+        }
+
+        static void ListVærdi(int i)
+        {
+            Console.WriteLine(i);
+        }
+        static void Skriv(int i)
+        {
+            Console.WriteLine("= "+i);
+        }
+    }
+}
